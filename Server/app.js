5bd6283d026081c630f037ee86b8541b96a097d4
Andrew <vurtua7@hotmail.com>
2018-03-01 14:48:59 -0500

    End of class push Thursday 3-1-2018

--- Server/app.js
+++ Server/app.js
@@ -1,10 +1,21 @@
 var io = require('socket.io')(process.envPort||3000);
 var shortid = require('shortid');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
 console.log("Server Started");
 //console.log(shortid.generate());
 
 var players = [];
+
+var dbObj;
+
+MongoClient.connect(url, function(err, client){
+	if(err)throw err;
+	
+	dbObj = client.db("SocketGameData");
+
+});
 
 io.on('connection', function(socket){
 	var thisPlayerId = shortid.generate();
@@ -33,4 +44,23 @@ io.on('connection', function(socket){
 		players.splice(players.indexOf(thisPlayerId),1);
 		socket.broadcast.emit('disconnected', {id:thisPlayerId});
 	});
+	
+	socket.on('send data', function(data){
+		console.log(JSON.stringify(data));
+		
+		dbObj.collection("playerData").save(data, function(err, res){
+			if(err)throw err;
+			console.log("data saved to MongoDB");
+		});
+	});
 });
+
+
+
+
+
+
+
+
+
+
